96e0c0281c724b31f8fe5a21dbdbf34892945c94
Irreducible-lucas <158491789+Irreducible-lucas@users.noreply.github.com>
2024-03-27 04:53:52 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+let selectField = document.getElementById("selectField")
+let selectText = document.getElementById("selectText")
+let options = document.getElementsByClassName("option")
+let list = document.getElementById("list")
+let arrowIcon = document.getElementById("arrow-icon")
+for (option of options){
+  option.onclick = function(){
+    selectText.innerHTML = this.textContent;
+    list.classList.toggle("hide")
+    arrowIcon.classList.toggle("rotate")
+  }
+}
+selectField.onclick = function(){
+list.classList.toggle("hide")
+arrowIcon.classList.toggle("rotate")
+}
